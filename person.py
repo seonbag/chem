ee101f77781b3408bc4b7b0f55b16d8e3294fb89
chem <petromonto@gmail.com>
2015-03-26 23:07:46 -0700

    Create person.py

---
+++ person.py
@@ -0,0 +1,24 @@
+# This project was me testing how inputs work
+# I might add to this program and make into an adventure game.
+
+name = raw_input('What is your name?\n') #asks for a name
+
+print 'Hi, %s.' % name
+print 'You know I always enjoy having you as a friend, %s' % name 
+
+job = raw_input('What are you doing?\n') #asks for what that person is doing
+
+print '%s sounds boring.' % job 
+print 'You should do something more intresting than %s' % job 
+
+age = raw_input('How old are you?\n') #asks for their age
+
+print 'You are pretty old. %s is a year of regrets and lies' % age #asks for their age
+print 'I want to go, you are bumming me out.'
+print 'You are so sad'
+print 'You seem to having nothing to live for in this life.'
+print 'Talk me later. OK?' 
+
+# end
+#i added spaces for easy reading... its a short program dont worry :)
+#chem
