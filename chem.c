c153d94171726ae40cb8f83379acee1d4101252a
chem <petromonto@gmail.com>
2015-03-26 22:55:03 -0700

    Create chem.c

---
+++ chem.c
@@ -0,0 +1,7 @@
+/*this isn't a hello world program bitch*/
+#include <stdio.h>
+int main(void) {
+printf("Hey dumbass, I still know C./n");
+printf("Shut the fuck up next time you talk shit about me./n");
+/*waht the fuck*/
+}
