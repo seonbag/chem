cc404cc46d48801905a32de2f2c57c6e7a312859
chem <petromonto@gmail.com>
2015-03-29 11:43:37 -0700

    Create minivan highway.py

---
+++ minivan highway.py
@@ -0,0 +1,12 @@
+#minivan highway
+print 'We are all riding in our minivan'
+print "Two drinks holders and a captain's chair"
+print "Pulling the lever to adjust the seat!"
+print "Electric windows, Electric doors."
+print "Four or five people sitting comfortably"
+print "Mini-van highway~"
+print "Mini-van highway~~"
+print "MMMMMMM HIGHWAY"
+print "IIIIIII HIGHWAY"
+print "NNNNNNN HIGHWAY"
+print "IIIIIII HIGHWAY"
