84f7f7e0ed33cadaff5e43202f57decdf9ce2395
chem <petromonto@gmail.com>
2015-03-29 11:49:43 -0700

    Update minivan highway.py

--- minivan highway.py
+++ minivan highway.py
@@ -1,4 +1,8 @@
 #minivan highway
+#hey fuck u its from a tim and eric sketch and i wanted to code it
+#its my github page not yours
+#not your NACHOS
+#Not yours
 print 'We are all riding in our minivan'
 print "Two drinks holders and a captain's chair"
 print "Pulling the lever to adjust the seat!"
